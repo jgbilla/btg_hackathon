539b668aec3c392008d1473861c47d65b4c04c55
Jean Ghislain Billa <billajean43@gmail.com>
2024-09-22 07:41:14 -0400

    Remove deps

--- modeling_cat.py
+++ modeling_cat.py
@@ -1,6 +1,5 @@
 import pandas as pd
-import numpy as np
-from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.model_selection import train_test_split
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import OneHotEncoder, LabelEncoder
 from sklearn.impute import SimpleImputer
@@ -12,10 +11,7 @@ import seaborn as sns
 from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
 from sklearn.ensemble import VotingClassifier
 from sklearn.linear_model import LogisticRegression
-from imblearn.over_sampling import SMOTE
-from sklearn.svm import SVC
-from scipy import stats
-from sklearn.preprocessing import RobustScaler
+
 
 # Load the data
 data = pd.read_csv('./data/compas-scores.csv')
